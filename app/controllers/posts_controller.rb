5b0b61393f7f2c9c0e14d4f459a70e15cf20b6c0
kodomo898 <kodomo893@gmail.com>
2018-09-04 17:15:59 +0900

    各form_tagの記述をform_forに修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,8 +11,8 @@ class PostsController < ApplicationController
   end
 
   def show
-    if @post = Post.find_by(id: params[:id])
-      @user = User.find_by(id: @post.user_id)
+    if @post = Post.find(params[:id])
+      @user = User.find(@post.user_id)
     else
       flash[:notice] = "投稿が見つかりません"
       redirect_to posts_path
@@ -20,7 +20,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content: params[:content], user_id: @current_user.id)
+    @post = Post.new(post_params)
+    @post.user_id = @current_user.id
     if @post.save
       flash[:notice] = "投稿しました"
       redirect_to posts_path
@@ -30,13 +31,12 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find_by(id: params[:id])
+    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find_by(id: params[:id])
-    @post.content = params[:content]
-    if @post.save
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
       flash[:notice] = "投稿を修正しました"
       redirect_to posts_path
     else
@@ -50,4 +50,13 @@ class PostsController < ApplicationController
     flash[:notice] = "削除しました"
     redirect_to posts_path
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :content
+    )
+  end
+
 end
